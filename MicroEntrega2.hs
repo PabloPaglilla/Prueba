e6f430f0aace31d11ccfd74463554ff8771aadbf
nicomelnyk <nicolasmelnyk@hotmail.com>
2018-05-20 21:09:32 -0300

    Se agregaron los casos de prueba

--- MicroEntrega2.hs
+++ MicroEntrega2.hs
@@ -126,6 +126,7 @@ acumuladoresYMemoriaEn0 :: Microprocesador -> Bool
 acumuladoresYMemoriaEn0 microprocesador = (acumuladorA microprocesador == 0) && (acumuladorB microprocesador == 0) &&
   (memoriaEn0 microprocesador)
 
+depurarPrograma :: Instruccion
 depurarPrograma microprocesador = microprocesador{programa =
   depuracionRecursiva (programa microprocesador) microprocesador []}
 
@@ -135,8 +136,8 @@ depuracionRecursiva :: Programa -> Microprocesador -> Programa -> Programa
 depuracionRecursiva [] _ programaDepurado = programaDepurado
 depuracionRecursiva programa microprocesador programaDepurado
   |acumuladoresYMemoriaEn0 ((siguienteInstruccion programa) microprocesador) =
-    depuracionRecursiva (instruccionesRestantes programa) ((siguienteInstruccion programa) microprocesador) programaDepurado
-  |otherwise = depuracionRecursiva (instruccionesRestantes programa) ((siguienteInstruccion programa) microprocesador) 
+    depuracionRecursiva (instruccionesRestantes programa) microprocesador programaDepurado
+  |otherwise = depuracionRecursiva (instruccionesRestantes programa) microprocesador 
     (programaDepurado ++ [(siguienteInstruccion programa)])
 
 primerosDosElementosOrdenados :: Memoria -> Bool
@@ -147,3 +148,80 @@ memoriaOrdenada microprocesador
   |length (memoria microprocesador) <= 1 = True
   |primerosDosElementosOrdenados (memoria microprocesador) = memoriaOrdenada microprocesador{memoria = tail (memoria microprocesador)}
   |otherwise = False
+  
+{- EJERCICIOS EN CONSOLA
+______________________________________________________________________
+CASOS DE PRUEBA
+______________________________________________________________________
+4.2 Punto 1
+(ejecutarPrograma.(cargarPrograma sumar10Y22)) xt8088 
+como precondiciones: el acumulador A y B están en cero
+como post-condiciones: el acumulador A tiene valor 32, el B cero y programCounter en 4
+*MicroEntrega1> (ejecutarPrograma.(cargarPrograma sumar10Y22)) xt8088
+Modelo: xt8088
+Program counter: 4
+Acumulador A: 32
+Acumulador B: 0
+Memoria: []
+Cantidad de instrucciones del programa: 4
+Mensaje de error :
+______________________________________________________________________
+4.2 Punto 2
+(ejecutarPrograma.(cargarPrograma dividir2Por0)) xt8088
+como precondiciones: el acumulador A y B están en cero
+como post-condiciones: el acumulador A tiene valor 2, el B cero y programCounter en 6. Debe haber mensaje de error dada la division por cero. Los primeros dos elementos de la memoria de datos deben ser 2 y 0
+*MicroEntrega1> (ejecutarPrograma.(cargarPrograma dividir2Por0)) xt8088
+Modelo: xt8088
+Program counter: 6
+Acumulador A: 2
+Acumulador B: 0
+Memoria: [2,0]
+Cantidad de instrucciones del programa: 6
+Mensaje de error : DIVISION BY ZERO
+______________________________________________________________________
+4.3 Punto 1
+(ifnz.(cargarPrograma [(lodv 3),swap])) fp20
+como precondiciones: el acumulador A esta en 7 y el B en 24
+como post-condiciones: el acumulador A tiene valor 24 y el B tiene 3
+*MicroEntrega1> (ifnz.(cargarPrograma [(lodv 3),swap])) fp20
+Modelo: fp20
+Program counter: 2
+Acumulador A: 24
+Acumulador B: 3
+Memoria: []
+Cantidad de instrucciones del programa: 2
+Mensaje de error : 
+______________________________________________________________________
+4.3 Punto 2
+(ifnz.(cargarPrograma [(lodv 3),swap])) xt8088
+como precondiciones: el acumulador A esta en 0 y el B en 
+como post-condiciones: el acumulador A y B deben quedar en cero
+*MicroEntrega1> (ifnz.(cargarPrograma [(lodv 3),swap])) xt8088
+Modelo: xt8088
+Program counter: 0
+Acumulador A: 0
+Acumulador B: 0
+Memoria: []
+Cantidad de instrucciones del programa: 2
+Mensaje de error : 
+______________________________________________________________________
+4.4
+Se depura un programa:
+*MicroEntrega1> (depurarPrograma.(cargarPrograma [swap,nop,(lodv 133),(lodv 0),(str 1 3),(str 2 0)])) xt8088
+Modelo: xt8088
+Program counter: 0
+Acumulador A: 0
+Acumulador B: 0
+Memoria: []
+Cantidad de instrucciones del programa: 2
+Mensaje de error : 
+Y me quedan dos instrucciones 
+______________________________________________________________________
+4.5
+Orden de la memoria: 
+*MicroEntrega1> memoriaOrdenada xt8088
+True
+*MicroEntrega1> memoriaOrdenada microDesorden
+False
+____________________________________________________________________
+-}
